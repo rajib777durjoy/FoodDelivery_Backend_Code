9565b43016f4ce9da1f8380a0427c228f126ced7
rajib777durjoy <rajib777chanda@gmail.com>
2025-12-23 23:07:51 +0600

    updata userModle in userRouter-userdata api function

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,19 +3,29 @@ import express from 'express'
 import { db } from '../config/db.js';
 import { users_table } from '../models/userModel.js';
 import { eq } from 'drizzle-orm';
+import { TokenVerify } from '../middleware/tokenVerifyMiddleware.js';
 
 export const userRouter = express.Router();
 userRouter.post('/user_data', async (req, res) => {
     const data = req.body;
-    const check_user= await db.select().from(users_table).where(eq(users_table.email,data.email));
-    if(check_user.length > 0){
-        return res.status(200).send({message:'you already user'})
+    const check_user = await db.select().from(users_table).where(eq(users_table.email, data.email));
+    if (check_user.length > 0) {
+        return res.status(200).send({ message: 'you already user' })
     }
     const user_data_save = await db.insert(users_table).values(data).returning();
     if (!user_data_save) {
         return res.status(500).send({ message: 'user data not save ' })
     }
     console.log('user db:', user_data_save);
-    res.status(200).send({message:true})
-     
+    res.status(200).send({ message: true })
+
+})
+
+userRouter.get('/user_data', TokenVerify, async (req, res) => {
+    const email = req.email;
+    const user = await db.select().from(users_table).where(eq(users_table.email,email));
+    if (user.length === 0) {
+        return res.status(500).send({ message: 'user is not match' })
+    }
+    res.status(200).send(user[0])
 })
---
+++ middleware/tokenVerifyMiddleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+export const TokenVerify = async(req,res,next)=>{
+const token = req.cookies.token;
+if(!token){
+return res.status(401).send({ message: "Unauthorized" });
+}
+  jwt.verify(token,process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Invalid token" });
+    }
+    req.email = decoded.email;
+    next();
+  });
+
+}
