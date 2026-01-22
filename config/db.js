61f1e253ce86cb01c73e644818a4c0aed0d26503
rajib777durjoy <rajib777chanda@gmail.com>
2026-01-22 23:49:49 +0600

    modify db.js file

--- config/db.js
+++ config/db.js
@@ -2,11 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.PSQL_DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 export const db = drizzle(pool);
+
 
