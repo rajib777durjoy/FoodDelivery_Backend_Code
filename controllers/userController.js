5dd999b2727c236c69de4be0c693fd1f0ff1b49e
rajib777durjoy <rajib777chanda@gmail.com>
2026-01-23 20:41:24 +0600

    try to solve error

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,10 +9,10 @@ import { order_table } from '../models/restaurantModel.js';
 
 export const userRouter = express.Router();
 
-userRouter.get('/check_db',async(req,res)=>{
-  const data= db.select().from(users_table).where(eq(users_table.email,'durjoy2001chando@gmail.com'));
-  console.log('check user data::',data)
-  res.send({message:'hello world !!'})
+userRouter.get('/check_db', async (req, res) => {
+  const users = await db.select().from(users_table).limit(2);
+  console.log('Users:', users);
+  res.send({ message: 'hello world !!' ,user:users})
 })
 userRouter.post('/user_data', async (req, res) => {
   const data = req.body;
@@ -31,7 +31,7 @@ userRouter.post('/user_data', async (req, res) => {
       .where(eq(users_table.email, data?.email));
 
     if (check_user.length > 0) {
-      return res.status(200).send({ message: 'You are already a user'});
+      return res.status(200).send({ message: 'You are already a user' });
     }
 
     // Insert new user safely
@@ -54,7 +54,7 @@ userRouter.post('/user_data', async (req, res) => {
 
     console.log('user data saved:', user_data_save[0]);
 
-    res.status(200).send({ message: true});
+    res.status(200).send({ message: true });
 
   } catch (err) {
     console.log('user_data error:', err);
@@ -63,41 +63,41 @@ userRouter.post('/user_data', async (req, res) => {
 });
 
 userRouter.get('/user_data', async (req, res) => {
-    const email = req.email;
-    console.log('email::', email)
-    const user = await db.select().from(users_table).where(eq(users_table.email, email));
-    console.log('get user ::', user)
-    if (user.length === 0) {
-        return res.status(500).send({ message: 'user is not match' })
-    }
-    res.status(200).send(user[0])
+  const email = req.email;
+  console.log('email::', email)
+  const user = await db.select().from(users_table).where(eq(users_table.email, email));
+  console.log('get user ::', user)
+  if (user.length === 0) {
+    return res.status(500).send({ message: 'user is not match' })
+  }
+  res.status(200).send(user[0])
 })
 
 userRouter.get('/deliver_man/:email', TokenVerify, async (req, res) => {
-    const { email } = req.params;
-    if (email !== req.email) {
-        return res.status(401).send({ message: 'Unauthorized user access ' })
-    }
-    const deliver_man = await db.select().from(users_table).where(eq(users_table.role, 'deliver_hero'));
+  const { email } = req.params;
+  if (email !== req.email) {
+    return res.status(401).send({ message: 'Unauthorized user access ' })
+  }
+  const deliver_man = await db.select().from(users_table).where(eq(users_table.role, 'deliver_hero'));
 
-    res.status(200).send(deliver_man);
+  res.status(200).send(deliver_man);
 })
 
 userRouter.get('/notifications/:user_id', async (req, res) => {
-    const id = parseInt(req.params.user_id);
-    const notification = await db.select().from(notification_table).where(eq(notification_table.user_id, id));
-    if (notification.length === 0) {
-        return res.status(400).send({ message: 'notification is not found !' })
-    }
-    res.status(200).send(notification);
+  const id = parseInt(req.params.user_id);
+  const notification = await db.select().from(notification_table).where(eq(notification_table.user_id, id));
+  if (notification.length === 0) {
+    return res.status(400).send({ message: 'notification is not found !' })
+  }
+  res.status(200).send(notification);
 })
 
 /// find the order details for notification page  ///
 userRouter.get('/notifications/order_details/:id', async (req, res) => {
-    const id = parseInt(req.params?.id);
-    const details = await db.select().from(order_table).where(eq(order_table.id, id));
-    if (details.length === 0) {
-        return res.status(400).send({ message: 'order details is not found !' })
-    }
-    res.status(200).send(details[0]);
+  const id = parseInt(req.params?.id);
+  const details = await db.select().from(order_table).where(eq(order_table.id, id));
+  if (details.length === 0) {
+    return res.status(400).send({ message: 'order details is not found !' })
+  }
+  res.status(200).send(details[0]);
 })
