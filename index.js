f5ed47e546f90e83c44dd80f7bd416798c540d1b
rajib777durjoy <rajib777chanda@gmail.com>
2026-01-24 14:25:43 +0600

    set cors origin

--- index.js
+++ index.js
@@ -17,15 +17,12 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app)
 const PORT = process.env.PORT || 5000;
+
 app.use(cors({
-    origin:'*',
-    credentials:true,
-}))
-// app.use(cors({
-//     origin:['https://eatnowfoodorderapp.vercel.app'],
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// }));
+    origin:['https://eatnowfoodorderapp.vercel.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 
 
