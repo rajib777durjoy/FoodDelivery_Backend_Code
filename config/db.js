d3d7da95d6e4dfc79b92eb3b1e7b376d911acfc7
rajib777durjoy <rajib777chanda@gmail.com>
2026-01-24 14:34:47 +0600

    set cors origin

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.PSQL_DATABASE_URL + "?sslmode=require",
+  connectionString: process.env.PSQL_DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 export const db = drizzle(pool);
