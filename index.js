874d04b5ccc723b67303fc09a59690a831504279
rajib777durjoy <rajib777chanda@gmail.com>
2026-04-10 21:41:30 +0600

    update server

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const server = http.createServer(app)
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['https://fooddelivery-704m.onrender.com',],
+    origin: ['https://fooddelivery-704m.onrender.com','http://localhost:5173'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
 }));
 app.use(cookieParser());
 app.use(express.json());
